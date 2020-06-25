8be1bd5816663da96c6f313a80bb56df4f2eba57
Stefano <stefanotucci89@gmail.com>
2020-06-25 09:19:55 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+    
+    $("nav a").click(function(e){
+        e.preventDefault();    
+        var distanza=$(this.hash).offset().top;
+ $("html,body").stop().animate({scrollTop:distanza},1000);
+        
+    });//$("nav a").click
+    
+    
+    $("#tornaSu").click(function(){
+        $("html,body").stop().animate({scrollTop:0},1000);
+    });
+    
+    
+    $(document).scroll(function(){
+                
+        console.log($(this).scrollTop());
+        
+        if($(this).scrollTop()>400){
+ $("#tornaSu").stop().animate({bottom:"20px"},500);
+            
+        }
+        else{
+ $("#tornaSu").stop().animate({bottom:"-100px"},500);
+        }
+
+    });//.scroll
+    
+
+    var tempo=1000;
+    
+    
+    $("#right").click(function(){
+        
+        
+       var immagineAttiva=$("img.attiva");
+        
+        $("#slider img").each(function(){
+            if(!$(this).hasClass("attiva")){
+                $(this).css("left","100%");
+            }
+        })
+                
+        if(immagineAttiva.next("img").length>0){            immagineAttiva
+            .next("img")
+            .animate({"left":"0"},tempo)
+            .addClass("attiva");
+        }
+        else{
+            $("#slider img:first-of-type").animate({left:0},tempo).addClass("attiva");
+        }
+        
+        immagineAttiva            
+            .animate({"left":"-100%"},tempo)
+             .removeClass("attiva");
+
+    });
+    
+    
+    $("#left").click(function(){
+                
+        
+       var immagineAttiva=$("img.attiva");
+        
+        $("#slider img").each(function(){
+            if(!$(this).hasClass("attiva")){
+                $(this).css("left","-100%");
+            }
+        })
+                
+        if(immagineAttiva.prev("img").length>0){            immagineAttiva
+            .prev("img")
+            .animate({"left":"0"},tempo)
+            .addClass("attiva");
+        }
+        else{
+            $("#slider img:last-of-type").animate({left:0},tempo).addClass("attiva");
+        }
+        
+        immagineAttiva            
+            .animate({"left":"100%"},tempo)
+             .removeClass("attiva");   
+
+        
+    });
+    
+    
+    
+/* slider */
+    
+    
+    
+
+    
+$("#slider").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+  nav: true,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "",   // String: Text for the "previous" button
+  nextText: "",       // String: Text for the "next" button
+//  maxwidth: "450",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+
+    
+$(".rslides_tabs a").text("");
+    
+    
+
+    
+});//.ready
